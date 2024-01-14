0150151fc3b547b28e1944d02bfbd0f101f39332
Banamali123 <banamalisinghmura9679305725@gmail.com>
2024-01-14 15:50:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/page/:page" element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
